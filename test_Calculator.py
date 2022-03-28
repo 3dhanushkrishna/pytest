cec45bf2270d8096bbf28ed88249e52c1207a384
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-28 10:12:07 +0530

    pytest

---
+++ test_Calculator.py
@@ -0,0 +1,21 @@
+import Calculator
+def test_add2no():
+    x=10
+    y=20
+    res=Calculator.add2no(x,y)
+    assert res == x+y
+def test_sub2no():
+    x=10
+    y=20
+    res=Calculator.sub2no(x,y)
+    assert res == x-y
+def test_mul2no():
+    x=10
+    y=20
+    res=Calculator.mul2no(x,y)
+    assert res == x*y
+def test_div2no():
+    x=10
+    y=20
+    res=Calculator.div2no(x,y)
+    assert res == x/y
