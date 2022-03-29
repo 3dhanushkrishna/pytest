76504da070039db2445f980983ad6d0e108a8fed
3dhanushkrishna <3dhanushkccd@gmail.com>
2022-03-29 10:36:08 +0530

    parametrize concepts

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,21 +1,23 @@
 import Calculator
-def test_add2no():
-    x=10
-    y=20
-    res=Calculator.add2no(x,y)
-    assert res == x+y
-def test_sub2no():
-    x=10
-    y=20
-    res=Calculator.sub2no(x,y)
-    assert res == x-y
-def test_mul2no():
-    x=10
-    y=20
-    res=Calculator.mul2no(x,y)
-    assert res == x*y
-def test_div2no():
-    x=10
-    y=20
-    res=Calculator.div2no(x,y)
-    assert res == x/y
+import pytest
+@pytest.mark.parametrize("a,b,c",[(5,2,7),(8,2,10),(1,2,3),(2,3,6)])
+def test_add2no(a,b,c):
+
+    res=Calculator.add2no(a,b)
+    assert res == c
+@pytest.mark.parametrize("a,b,c",[(5,2,3),(8,2,6),(1,2,3),(4,3,6)])
+def test_sub2no(a,b,c):
+
+    res=Calculator.sub2no(a,b)
+    assert res == c
+@pytest.mark.parametrize("a,b,c",[(5,2,10),(8,2,16),(1,2,3),(4,3,6)])
+def test_mul2no(a,b,c):
+
+    res=Calculator.mul2no(a,b)
+    assert res == c
+@pytest.mark.parametrize("a,b,c",[(6,2,3),(8,2,4),(6,2,8),(18,3,7)])
+@pytest.mark.muldiv
+def test_div2no(a,b,c):
+
+    res=Calculator.div2no(a,b)
+    assert res ==c
